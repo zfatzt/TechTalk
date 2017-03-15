e148d333a6a7731c71a02f46a22604a6ffa71faa
zfatzt <zfatzt@WS-ZFATZT.bbcnet-zh.local>
2017-03-15 14:58:55 +0100

    Datenbankverbindung

--- controller/UserController.php
+++ controller/UserController.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once '../repository/UserRepository.php';
+
 /**
  * Siehe Dokumentation im DefaultController.
  */
@@ -7,15 +9,44 @@ class UserController
 {
     public function index()
     {
-        // Anfrage an die URI /user/crate weiterleiten (HTTP 302)
-        header('Location: /user/create');
+        $userRepository = new UserRepository();
+
+        $view = new View('user_index');
+        $view->title = 'Benutzer';
+        $view->heading = 'Benutzer';
+        $view->users = $userRepository->readAll();
+        $view->display();
     }
 
     public function create()
     {
-        $view = new View('user_form');
+        $view = new View('user_create');
         $view->title = 'Benutzer erstellen';
         $view->heading = 'Benutzer erstellen';
         $view->display();
+    }
+
+    public function doCreate()
+    {
+        if ($_POST['send']) {
+            $benutzerName = $_POST['benutzerName'];
+            $email = $_POST['email'];
+            $passwort = $_POST['passwort'];
+
+            $userRepository = new UserRepository();
+            $userRepository->create($benutzerName, $email, $passwort);
+        }
+
+        // Anfrage an die URI /user weiterleiten (HTTP 302)
+        header('Location: /user');
+    }
+
+    public function delete()
+    {
+        $userRepository = new UserRepository();
+        $userRepository->deleteById($_GET['id']);
+
+        // Anfrage an die URI /user weiterleiten (HTTP 302)
+        header('Location: /user');
     }
 }
