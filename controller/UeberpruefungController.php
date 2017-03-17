bf930dd0e260fc2e811e2d73f4c146f02d54364a
zfatzt <zfatzt@WS-ZFATZT.bbcnet-zh.local>
2017-03-17 10:11:21 +0100

    Registration mit Gui verknüpft, Anmeldung, Datenbank überprüfung

--- controller/UeberpruefungController.php
+++ controller/UeberpruefungController.php
@@ -1,39 +1,72 @@
-<?php 
-
+<?php
 require_once '../repository/UserRepository.php';
 require_once '../repository/LoginResult.php';
-
-class UeberpruefungController{
-
-	
+class UeberpruefungController {
 	public function anmelden() {
-		$_SESSION ["dsz"] = 'Falsch';
 		if ($_SERVER ["REQUEST_METHOD"] == "POST") {
 			
-			$passwort = $_POST ['password'];
-			if (empty ( $passwort )) {
-				$passwortErr = "Bitte geben Sie ein Passwort ein!";
-			}
-			
-			if (empty ( $_POST ["email"] )) {
+			if (empty ( $_POST ["anmeldeEmail"] )) {
 				$emailErr = "Bitte geben Sie eine E-Mail Adresse ein!";
 			} else {
-				$email = test_input ( $_POST ["email"] );
+				$email = ($_POST ["anmeldeEmail"]);
 				// check if e-mail address is well-formed
 				if (! filter_var ( $email, FILTER_VALIDATE_EMAIL )) {
 					$emailErr = "Bitte geben Sie eine gueltige E-Mail Adresse ein!";
 				}
 			}
+			
+			$passwort = $_POST ['anmeldePasswort'];
+			if (empty ( $passwort )) {
+				$passwortErr = "Bitte geben Sie ein Passwort ein!";
+			}
+			
 			$userRepository = new UserRepository ();
-			$loginResult = $userRepository->existiertNutzer ( $email, $passwort );
-			if ($loginResult->getBuntzerExistiert ()) {
-				$_SESSION ["benutzerName"] = $loginResult->getUserName ();
+			$loginResult = $userRepository->existiertNutzer( $email, $passwort );
+			if ($loginResult->getBuntzerExistiert () && $loginResult->getBuntzerKannEinloggen()) {
+				$_SESSION ["benutzername"] = $loginResult->getBenutzername ();
+				header ( "Location: /" );
+				exit ();
 			} else {
-				$_SESSION ["benutzerName"] = 'Falsch';
+				$_SESSION ["benutzername"] = 'Falsch';
 			}
 		}
-		header ( "Location: /" );
-		exit ();
 	}
+	public function registrieren() {
+		if ($_SERVER ["REQUEST_METHOD"] == "POST") {
+			
+			if (empty ( $_POST ["email"] )) {
+				$emailErr = "Bitte geben Sie eine E-Mail Adresse ein!";
+			} else {
+				$email = $_POST ["email"];
+				// check if e-mail address is well-formed
+				if (! filter_var ( $email, FILTER_VALIDATE_EMAIL )) {
+					$emailErr = "Bitte geben Sie eine gueltige E-Mail Adresse ein!";
+				}
+				
+				$benutzername = $_POST ['benutzername'];
+				if (empty ( $benutzername )) {
+					$benutzerNameErr = "Bitte geben Sie einen Benutzernamen ein!";
+				}
+				
+				$passwort = $_POST ['password'];
+				if (empty ( $passwort )) {
+					$passwortErr = "Bitte geben Sie ein Passwort ein!";
+				}
+			}
+			
+			$userRepository = new UserRepository ();
+			$userRepository->benutzerErstellen($benutzername, $email, $passwort );
+			
+			
+			
+			
+		}
+		
+		// header ( "Location: /" );
+		// exit ();
+	}
+	
+	// // Anfrage an die URI /user weiterleiten (HTTP 302)
+	// header ( 'Location: /user' );
 }
-	?>
+?>
--- view/benutzer_anmelden.php
+++ view/benutzer_anmelden.php
@@ -1,46 +1,151 @@
-
 <div class="container">
-	<button type="button" class="btn btn-info btn-lg" data-toggle="modal"
-		data-target="#myModal">Anmelden</button>
-	<div class="modal fade" id="myModal" role="dialog">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal">&times;</button>
-					<h4 class="modal-title">Anmelden</h4>
+	<br>
+	<button class="btn btn-primary btn-lg" href="#signup"
+		data-toggle="modal" data-target=".bs-modal-sm">Anmelden/Registrieren</button>
+	<br>
+</div>
+<!-- Modal -->
+<div class="modal fade bs-modal-sm" id="myModal" tabindex="-1"
+	role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<br>
+			<div class="bs-example bs-example-tabs">
+				<ul id="myTab" class="nav nav-tabs">
+					<li class="active"><a href="#signin" data-toggle="tab">Anmelden</a></li>
+					<li class=""><a href="#signup" data-toggle="tab">Registrieren</a></li>
+				</ul>
+			</div>
+			<div class="modal-body">
+				<div id="myTabContent" class="tab-content">
+					<div class="tab-pane fade active in" id="signin">
+						<form class="form-horizontal" method="post" action="/ueberpruefung/anmelden">
+							<fieldset>
+								<!-- Sign In Form -->
+								<!-- Text input-->
+								<div class="control-group">
+									<label class="control-label" for="anmeldeEmail">Email:</label>
+									<div class="controls">
+										<input required="" id="anmeldeEmail" name="anmeldeEmail" type="email"
+											class="form-control" placeholder="max@mustermann.ch"
+											class="input-medium" required="">
+									</div>
+								</div>
+
+								<!-- Password input-->
+								<div class="control-group">
+									<label class="control-label" for="passwordinput">Passwort:</label>
+									<div class="controls">
+										<input required="" id="anmeldePasswort" name="anmeldePasswort"
+											class="form-control" type="password" placeholder="********"
+											class="input-medium">
+									</div>
+								</div>
+
+								<!-- Multiple Checkboxes (inline) -->
+								<div class="control-group">
+									<label class="checkbox" for="rememberme"></label>
+									<div class="controls">
+										<label class="checkbox" for="rememberme-0"> <input
+											type="checkbox" name="rememberme" id="rememberme-0"
+											value="Remember me"> Angemeldet bleiben
+										</label>
+									</div>
+								</div>
+
+								<!-- Button -->
+								<div class="control-group">
+									<label class="control-label" for="signin"></label>
+									<div class="controls">
+										<button id="signin" type="submit" name="signin" class="btn btn-success">Einloggen
+										</button>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+					
+					
+					
+					<div class="tab-pane fade" id="signup"> <!-- registrieren -->
+						<form class="form-horizontal" method="post" action="/ueberpruefung/registrieren">
+							<fieldset>
+								<!-- Sign Up Form -->
+								<!-- Text input-->
+								<div class="control-group">
+									<label class="control-label" for="email">Email:</label>
+									<div class="controls">
+										<input id="email" name="email" class="form-control"
+											type="email" placeholder="max@mustermann.ch"
+											class="input-large" required="">
+									</div>
+								</div>
+
+								<!-- Text input-->
+								<div class="control-group">
+									<label class="control-label" for="benutzername">Benutzername:</label>
+									<div class="controls">
+										<input id="benutzername" name="benutzername" class="form-control"
+											type="text" placeholder="Max457" class="input-large"
+											required="">
+									</div>
+								</div>
+
+								<!-- Password input-->
+								<div class="control-group">
+									<label class="control-label" for="password">Passwort:</label>
+									<div class="controls">
+										<input id="password" name="password" class="form-control"
+											type="password" placeholder="********" class="input-large"
+											required=""> <em>1-8 Characters</em>
+									</div>
+								</div>
+
+								<!-- Text input-->
+								<div class="control-group">
+									<label class="control-label" for="reenterpassword"> Passwort
+										wiederholen:</label>
+									<div class="controls">
+										<input id="reenterpassword" class="form-control"
+											name="reenterpassword" type="password" placeholder="********"
+											class="input-large" required="">
+									</div>
+								</div>
+
+								<!-- Multiple Radios (inline) -->
+								<br>
+								<div class="control-group">
+									<div class="controls">
+										<label class="checkbox" for="humancheck-0"> <input
+											type="radio" name="humancheck" id="humancheck-0"
+											value="robot" checked="checked">Ich bin ein Computer
+										</label> <label class="checkbox" for="humancheck-1"> <input
+											type="radio" name="humancheck" id="humancheck-1"
+											value="human">Ich bin ein Mensch
+										</label>
+									</div>
+								</div>
+
+								<!-- Button -->
+								<div class="control-group">
+									<label class="control-label" for="confirmsignup"></label>
+									<div class="controls">
+										<button id="confirmsignup" name="confirmsignup" type="submit"
+											class="btn btn-success">Registrieren</button>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
 				</div>
-				<form class="form-signin" method="post" action="/ueberpruefung/anmelden">
-					<div class="modal-body">
-
-						<div class="row">
-							<div class="col-xs-5">
-								<div class="input-group">
-									<span class="input-group-addon"><span
-										class="glyphicon glyphicon-user"></span></span> <input
-										type="email" class="form-control" placeholder="Email">
-								</div>
-							</div>
-						</div>
-						<br>
-						<div class="row">
-							<div class="col-xs-5">
-								<div class="input-group">
-									<span class="input-group-addon"><span
-										class="glyphicon glyphicon-lock"></span></span> <input
-										type="password" class="form-control" placeholder="Passwort">
-								</div>
-							</div>
-						</div>
-
-					</div>
-					<div class="modal-footer">
-						<button type="submit" class="btn btn-default">Absenden</button>
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					</div>
-				</form>
+			</div>
+			<div class="modal-footer">
+				<center>
+					<button type="button" class="btn btn-default" data-dismiss="modal">Abbrechen</button>
+				</center>
 			</div>
 		</div>
 	</div>
-
 </div>
-
+</body>
+</html>
