42a5aa30449c78d30c17ef7c6e26b3815179dfdd
zmoucl <zmoucl@WS-ZMOUCL.bbcnet-zh.local>
2017-03-22 09:53:36 +0100

    Chat start

--- view/chat_bitcoin.php
+++ view/chat_bitcoin.php
@@ -1,4 +1,14 @@
-<body>
-	<h3>Hello World</h3>
-
-</body>
+<h3>Bitcoin Chat</h3>
+<div>
+	<div id="chatbox">Hallo</div>
+	<div id="nachricht" class="col-lg-5">
+		<div class="input-group">
+			<input type="text" class="form-control"
+				placeholder="Nachricht eingeben..."> <span class="input-group-btn">
+				<button id="absendenButton" class="btn btn-success" type="button">
+					<span class="glyphicon glyphicon-send"></span>
+				</button>
+			</span>
+		</div>
+	</div>
+</div>
