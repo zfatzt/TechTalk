6ba2a173e23764ef48e35014c265206cb2c93039
zfatzt <zfatzt@WS-ZFATZT.bbcnet-zh.local>
2017-03-22 11:21:25 +0100

    Fehlerbehebungen

--- controller/UeberpruefungController.php
+++ controller/UeberpruefungController.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 require_once '../repository/UserRepository.php';
 require_once '../repository/LoginResult.php';
 class UeberpruefungController {
@@ -9,7 +9,6 @@ class UeberpruefungController {
 				$emailErr = "Bitte geben Sie eine E-Mail Adresse ein!";
 			} else {
 				$email = htmlspecialchars($_POST ["anmeldeEmail"]);
-				// check if e-mail address is well-formed
 				if (! filter_var ( $email, FILTER_VALIDATE_EMAIL )) {
 					$emailErr = "Bitte geben Sie eine gueltige E-Mail Adresse ein!";
 				}
@@ -21,33 +20,32 @@ class UeberpruefungController {
 			}
 			
 			$userRepository = new UserRepository ();
-			$loginResult = $userRepository->existiertNutzer( $email, $passwort );
+			$loginResult = $userRepository->existiertNutzer($email, $passwort );
 			if ($loginResult->getBuntzerExistiert () && $loginResult->getBuntzerKannEinloggen()) {
 				$_SESSION ["benutzername"] = $loginResult->getBenutzername ();
 				header ( "Location: /" );
 				exit ();
 			} else {
-				header("Location: /user/anmelden");
+				header("Location: /");
 			}
 		}
 	}
 	public function registrieren() {
 		if ($_SERVER ["REQUEST_METHOD"] == "POST") {
 			
-			if (empty ( $_POST ["email"] )) {
-				$emailErr = "Bitte geben Sie eine E-Mail Adresse ein!";
-			}
-				$benutzername = $_POST ['benutzername'];
+				$email = htmlspecialchars( $_POST ["email"] );
+	
+				$benutzername = htmlspecialchars($_POST ['benutzername']);
 				if (empty ( $benutzername )) {
 					$benutzerNameErr = "Bitte geben Sie einen Benutzernamen ein!";
 				}
 				
-				$passwort = $_POST ['password'];
+				$passwort = htmlspecialchars($_POST ['password']);
 				if (empty ( $passwort )) {
 					$passwortErr = "Bitte geben Sie ein Passwort ein!";
 				}
 				
-				$passwortWiederholen = $_POST ['reenterpassword'];
+				$passwortWiederholen = htmlspecialchars($_POST ['reenterpassword']);
 				if (empty ( $passwortWiederholen )) {
 					$passwortErr = "Bitte geben Sie ein Passwort ein!";
 				}
@@ -57,11 +55,11 @@ class UeberpruefungController {
 			}
 			
 			if ($passwort === $passwortWiederholen && $computercheck == "human"){
-			
 				$userRepository = new UserRepository ();
 				$userRepository->benutzerErstellen($benutzername, $email, $passwort );
-			}else {
 				
+				
+			}else {
 				$view = new View ( 'benutzer_anmelden' );
 				$view->title = 'Startseite';
 				$view->heading = 'Startseite';
